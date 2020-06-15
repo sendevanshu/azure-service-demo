d3406f9cf67766059e6bdf466b09528700d66f29
mayank4java <mayank4java@gmail.com>
2020-06-15 23:22:41 +0530

    Added healthcheck controller

---
+++ src/main/java/com/mayank4java/controller/HealthCheckController.java
@@ -0,0 +1,16 @@
+package com.mayank4java.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+	@RequestMapping("/sayHello/{firstName}")
+	public String sayHello(@PathVariable("firstName") String name){
+		
+		return "Hello " + name+" !";
+	}
+	
+}
