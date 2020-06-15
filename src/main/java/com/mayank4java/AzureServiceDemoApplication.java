920a8d3a72b768dc81d73af2d4da7ec0358c2288
mayank4java <mayank4java@gmail.com>
2020-06-15 23:02:12 +0530

    Inital commit

---
+++ src/main/java/com/mayank4java/AzureServiceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.mayank4java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureServiceDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureServiceDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mayank4java/AzureServiceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayank4java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureServiceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
